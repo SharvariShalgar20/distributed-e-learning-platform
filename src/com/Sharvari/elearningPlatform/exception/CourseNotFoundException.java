7e93379faf439654de299483b75353a0afdf4c19
SharvariShalgar20 <shalgarsharvari@gmail.com>
2026-02-19 23:19:52 +0530

    exception package implemented

---
+++ src/com/Sharvari/elearningPlatform/exception/CourseNotFoundException.java
@@ -0,0 +1,8 @@
+package com.Sharvari.elearningPlatform.exception;
+
+public class CourseNotFoundException extends RuntimeException{
+
+    public CourseNotFoundException(String message) {
+        super(message);
+    }
+}
