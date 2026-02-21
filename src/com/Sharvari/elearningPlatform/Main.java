dfed195426ebae6df5ce4fe95564b20883a3ae7d
SharvariShalgar20 <shalgarsharvari@gmail.com>
2026-02-21 22:56:17 +0530

    feat : init method created in main

--- src/com/Sharvari/elearningPlatform/Main.java
+++ src/com/Sharvari/elearningPlatform/Main.java
@@ -9,8 +9,22 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static final Scanner scanner = new Scanner(System.in);
+    private static UserService       userService;
+    private static CourseService     courseService;
+    private static EnrollmentService enrollmentService;
+    private static QuizService       quizService;
+    private static User currentUser = null;
+
     public static void main(String[] args) {
 
+        initServices();
+    }
 
+    private static void initServices() {
+        userService       = new UserService();
+        courseService     = new CourseService(userService);
+        enrollmentService = new EnrollmentService(userService, courseService);
+        quizService       = new QuizService(userService, courseService, enrollmentService);
     }
 }
