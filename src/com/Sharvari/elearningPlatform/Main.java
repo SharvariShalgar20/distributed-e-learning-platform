46955a2953a6b1373f699a571df8b62caf56bdee
SharvariShalgar20 <shalgarsharvari@gmail.com>
2026-02-21 22:58:10 +0530

    feat : loadDemoData() in Main

--- src/com/Sharvari/elearningPlatform/Main.java
+++ src/com/Sharvari/elearningPlatform/Main.java
@@ -19,6 +19,7 @@ public class Main {
     public static void main(String[] args) {
 
         initServices();
+        loadDemoData();
     }
 
     private static void initServices() {
@@ -27,4 +28,24 @@ public class Main {
         enrollmentService = new EnrollmentService(userService, courseService);
         quizService       = new QuizService(userService, courseService, enrollmentService);
     }
+
+    private static void loadDemoData() {
+        System.out.println("\n  Loading demo data...");
+        userService.loadDemoData();
+        List<Instructor> instructors = userService.getAllInstructors();
+        String drSarahId  = instructors.get(0).getUserId();
+        String profMarkId = instructors.get(1).getUserId();
+        courseService.loadDemoData(drSarahId, profMarkId);
+        List<Course> courses = courseService.getPublishedCourses();
+        quizService.loadDemoData(drSarahId, courses.get(0).getCourseId(), courses.get(1).getCourseId());
+        System.out.println("  ──────────────────────────────────────────");
+        System.out.println("  Demo Logins:");
+        System.out.println("   Student    → alice@email.com / alice123");
+        System.out.println("   Student    → bob@email.com   / bob123");
+        System.out.println("   Instructor → sarah@email.com / sarah123");
+        System.out.println("   Instructor → mark@email.com  / mark123");
+        System.out.println("  ──────────────────────────────────────────");
+    }
+
+
 }
