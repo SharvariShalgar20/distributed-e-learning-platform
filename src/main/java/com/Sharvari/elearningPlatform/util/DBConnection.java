3c3804b767ea0803f55b83dc54ce577cca9af6eb
SharvariShalgar20 <shalgarsharvari@gmail.com>
2026-04-09 20:05:03 +0530

    Warning added

--- src/main/java/com/Sharvari/elearningPlatform/util/DBConnection.java
+++ src/main/java/com/Sharvari/elearningPlatform/util/DBConnection.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
+    //create database in MySQL first
     private static final String URL      = "jdbc:mysql://localhost:3306/elearning_platform";
     private static final String USER     = "root";
     private static final String PASSWORD = "S..";
